1304f3d305409bd4480311dad085323cc05bc370
Charlotte Fereday <cbrfereday@gmail.com>
2017-06-06 11:31:20 +0100

    Deletes set up files

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,7 +1,14 @@
-describe('fizzBuzz challenge', function() {
-  var fizzBuzz = new fizzBuzz();
 
-  it('should return fizzbuzz if num divided by 3', function() {
-    expect(fizzBuzz.play(3)).toEqual('Fizz');
-  });
-});
+
+
+
+
+
+
+// describe('fizzBuzz challenge', function() {
+//   var fizzBuzz = new fizzBuzz();
+//
+//   it('should return fizzbuzz if num divided by 3', function() {
+//     expect(fizzBuzz.play(3)).toEqual('Fizz');
+//   });
+// });
